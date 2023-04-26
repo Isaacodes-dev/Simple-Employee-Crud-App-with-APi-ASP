43b5dccf53510655c71c7be73dd525bfe29095db
Isaacodes-dev <mulengaisaac10@gmail.com>
2023-04-26 13:25:51 +0300

    Add project files.

---
+++ TrailApp/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrailApp.Models.Domain;
+
+namespace TrailApp.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
